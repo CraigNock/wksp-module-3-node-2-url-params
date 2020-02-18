efc18bdaee8f414f2c60e66d0a89f267965124cd
Craig Nockels <cnockels@gmail.com>
2020-02-18 16:31:12 -0500

    ex1_done

--- server.js
+++ server.js
@@ -17,20 +17,39 @@ app.set('view engine', 'ejs');
 
 // endpoints here
 
+//top 50
 app.get('/top50', (req, res) => {
     res.render('pages/top50', {
         title: 'Top 50 Songs Streamed on Spotify',
         top50: top50
     });
 });
-
+//top artist
 app.get('/top50/popular-artist', (req, res) => {
     res.render('pages/popular-artist', {
         title: 'Top Artist Streamed on Spotify',
         top50: top50
     });
 });
+//song//#
+app.get('/song/:number', (req, res) => {
+    let number = req.params.number;
+    let selectSong = top50.filter((item) => item.rank == number);
+    if (number > 0 && number <51){
+        res.render('pages/song#', {
+            title: `Song #${number}`,
+            selectSong: selectSong[0]
+        });
+    } else {
+        res.status(404);
+        res.render('pages/fourOhFour', {
+        title: 'I got nothing',
+        path: req.originalUrl
+    });
+    }
+});
 
+//homepage
 app.get('/', (req, res) => {
     res.send('hi');
 });
